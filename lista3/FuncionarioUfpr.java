62666bb0d30037c868b7c61079c1feb0f05e0080
DuCostaBrito <edu.brito02@hotmail.com>
2022-12-15 13:16:47 -0300

    finished constructor

--- lista3/FuncionarioUfpr.java
+++ lista3/FuncionarioUfpr.java
@@ -4,11 +4,17 @@ public class FuncionarioUfpr {
     //Atributos
     private String nome;
     private int matricula;
-    Data dataNascimento;
+    Data dataNasc;
     private String cargo;
 
     //Construtor
-
+    public FuncionarioUfpr(String nome, int matricula, Data dataNascimento, String cargo) {
+        setNome(nome);
+        setMatricula(matricula);
+        this.dataNasc = dataNascimento;
+        setCargo(cargo);
+    }
+    
     //Metodos get/set
     public String getNome() {
         return this.nome;
