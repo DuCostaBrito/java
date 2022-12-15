5c1ecb775b7d8fbb17ecdb7616faee14dd032c76
DuCostaBrito <edu.brito02@hotmail.com>
2022-12-15 15:59:28 -0300

    finished Ingresso

--- lista4/Ex2/Ingresso.java
+++ lista4/Ex2/Ingresso.java
@@ -1,5 +1,25 @@
 package lista4.Ex2;
 
 public class Ingresso {
-    
+    // Atributos
+    protected int valor;
+
+    // Construtores
+    public Ingresso() {
+    }
+
+    // Metodos get/set
+    public int getValor() {
+        return this.valor;
+    }
+
+    public void setValor(int valor) {
+        if (valor > 0)
+            this.valor = valor;
+    }
+
+    // Metodos
+    public void imprimir() {
+        System.out.println("Valor do ingresso: " + this.getValor());
+    }
 }
