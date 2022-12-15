4313aebb0bb7c54a305fddc31c72da8dcc05452a
DuCostaBrito <edu.brito02@hotmail.com>
2022-12-15 16:05:35 -0300

    finished IngressoVip

--- lista4/Ex2/IngressoVip.java
+++ lista4/Ex2/IngressoVip.java
@@ -1,5 +1,25 @@
 package lista4.Ex2;
 
-public class IngressoVip {
-    
+public class IngressoVip extends Ingresso{
+    //Atributos
+    private int valorAdicional;
+
+    //Construtores
+    public IngressoVip(){
+    }
+
+    //Metodos get/set
+    public int getValorAdicional() {
+        return this.valorAdicional;
+    }
+
+    public void setValorAdicional(int valorAdicional) {
+        if (valorAdicional > 0)
+            this.valorAdicional = valorAdicional;
+    }
+
+    //Metodos Sobrepostos
+    public int getValor() {
+        return this.valor + this.valorAdicional;
+    }
 }
