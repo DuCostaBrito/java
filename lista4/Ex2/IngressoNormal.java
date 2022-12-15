8136c958c3bfb3317725eb63c239f68cfc91419b
DuCostaBrito <edu.brito02@hotmail.com>
2022-12-15 16:08:50 -0300

    finished IngressoNormal

--- lista4/Ex2/IngressoNormal.java
+++ lista4/Ex2/IngressoNormal.java
@@ -1,5 +1,14 @@
 package lista4.Ex2;
 
-public class IngressoNormal {
-    
+public class IngressoNormal extends Ingresso{
+    //Atributos
+
+    //Construtores
+    public IngressoNormal(){}
+
+    //Metodos
+    public void imprimir() {
+        System.out.println("Ingresso Normal");
+        super.imprimir();
+    }
 }
