aa3d92d1f663929117341c6316fca9cf70219e11
DuCostaBrito <edu.brito02@hotmail.com>
2022-12-15 15:24:15 -0300

    finished Lampada

--- lista4/Ex1/Lampada.java
+++ lista4/Ex1/Lampada.java
@@ -1,5 +1,19 @@
 package lista4.Ex1;
 
 public class Lampada {
-    
+    //Atributos
+    protected boolean ligado;
+
+    //metodos get/set
+    public boolean isLigado(){
+        return this.ligado;
+    }
+
+    public void setLigado() {
+        this.ligado = true;
+    }
+
+    public void setDesligado() {
+        this.ligado = false;
+    }
 }
