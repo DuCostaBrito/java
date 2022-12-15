056d3d79692bec1ca38ceb1e2f61a2e349e118c9
DuCostaBrito <edu.brito02@hotmail.com>
2022-12-15 13:29:22 -0300

    finished FolhaDePagamentoUFPR

--- lista3/FolhaDePagamentoUFPR.java
+++ lista3/FolhaDePagamentoUFPR.java
@@ -1,5 +1,27 @@
 package lista3;
 
 public class FolhaDePagamentoUFPR {
+    //Atributos
+    FuncionarioUfpr funcionario;
+    Data dataPagamento;
+    private double salario;
+
+    //Construtor
+    public FolhaDePagamentoUFPR(FuncionarioUfpr funcionario, int mes, int ano, double salario) {
+        this.funcionario = funcionario;
+        this.dataPagamento = new Data(mes, ano);
+        this.salario = salario;
+    }
+
+    //Metodos get/set
+    public double getSalario() {
+        return this.salario;
+    }
+
+    public void setSalario(double salario) {
+        if (salario > 0)
+            this.salario = salario;
+    }
+
     
 }
