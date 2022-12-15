315eca05876fff64426b37cc830dd9e01ea6f987
DuCostaBrito <edu.brito02@hotmail.com>
2022-12-15 18:33:30 -0300

    finished FuncionarioMensalista

---
+++ AulaPratica1/FuncionarioMensalista.java
@@ -0,0 +1,17 @@
+package AulaPratica1;
+
+public class FuncionarioMensalista extends Funcionario{
+    //Atributos
+    private int salario;
+
+    //Construtores
+
+    //Metodos get/set
+    public int getSalario() {
+        return this.salario;
+    }
+
+    public void setSalario(int salario) {
+        this.salario = salario;
+    }
+}
