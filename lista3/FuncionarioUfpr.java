dfd490233b85de3339625e46d107d7a78db61c4f
DuCostaBrito <edu.brito02@hotmail.com>
2022-12-15 13:11:54 -0300

    finished FuncionarioUfpr

--- lista3/FuncionarioUfpr.java
+++ lista3/FuncionarioUfpr.java
@@ -1,5 +1,38 @@
 package lista3;
 
 public class FuncionarioUfpr {
-    
+    //Atributos
+    private String nome;
+    private int matricula;
+    Data dataNascimento;
+    private String cargo;
+
+    //Construtor
+
+    //Metodos get/set
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getMatricula() {
+        return this.matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getCargo() {
+        return this.cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+
 }
