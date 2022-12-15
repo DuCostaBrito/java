b1556f6d43c0316186d1d5c49434c0c4d49b9e41
DuCostaBrito <edu.brito02@hotmail.com>
2022-12-15 13:16:24 -0300

    finished Data

--- lista3/Data.java
+++ lista3/Data.java
@@ -1,5 +1,15 @@
 package lista3;
 
 public class Data {
-    
+    //Atributos
+    int dia;
+    int mes;
+    int ano;
+
+    //Construtor
+    public Data(int dia, int mes, int ano) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
 }
