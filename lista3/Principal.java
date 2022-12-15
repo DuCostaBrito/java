fa2ba1cae875efe12a9f065581e0264e0c83e24e
DuCostaBrito <edu.brito02@hotmail.com>
2022-12-15 13:53:19 -0300

    finished initializing funcionario

--- lista3/Principal.java
+++ lista3/Principal.java
@@ -1,5 +1,33 @@
 package lista3;
 
+import java.util.Scanner;
+
 public class Principal {
-    
+    public static void main(String args[]) {
+        String nome, cargo;
+        int dia, mes, ano, matricula;
+        double salario;
+
+        FuncionarioUfpr[] funcionarios = new FuncionarioUfpr[3];
+        Scanner input = new Scanner(System.in);
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Digite o nome do funcionario: ");
+            nome = input.nextLine();
+
+            System.out.print("Digite o cargo do funcionario: ");
+            cargo = input.nextLine();
+
+            System.out.print("Digite a data de nascimento do funcionario: ");
+            dia = input.nextInt();
+            mes = input.nextInt();
+            ano = input.nextInt();
+
+            System.out.print("Digite a matricula do funcionario: ");
+            matricula = input.nextInt();
+
+            funcionarios[i] = new FuncionarioUfpr(nome, matricula, dia, mes, ano, cargo);
+        }
+        
+    }
 }
