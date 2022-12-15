cef9815eb4736a72b25cf919dd255e9cead6da34
DuCostaBrito <edu.brito02@hotmail.com>
2022-12-15 15:27:17 -0300

    finished Fluorescente

--- lista4/Ex1/Fluorescente.java
+++ lista4/Ex1/Fluorescente.java
@@ -1,5 +1,17 @@
 package lista4.Ex1;
 
-public class Fluorescente {
-    
+public class Fluorescente extends Lampada{
+    //Atributos
+    private int comprimento;
+
+
+    //Metodos get/set
+    public int getComprimento() {
+        return this.comprimento;
+    }
+
+    public void setComprimento(int comprimento) {
+        if (comprimento > 0)
+            this.comprimento = comprimento;
+    }
 }
