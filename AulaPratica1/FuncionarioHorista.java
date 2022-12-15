16e2076a74768f3ef235a11d4bf4ec0129d5d7e2
DuCostaBrito <edu.brito02@hotmail.com>
2022-12-15 18:37:31 -0300

    finished FuncionarioHorista

---
+++ AulaPratica1/FuncionarioHorista.java
@@ -0,0 +1,26 @@
+package AulaPratica1;
+
+public class FuncionarioHorista extends Funcionario{
+    //Atributos
+    private int valorHora;
+    private int numHoras;
+    
+    //Construtores
+
+    //Metodos get/set
+    public int getValorHora() {
+        return this.valorHora;
+    }
+
+    public void setValorHora(int valorHora) {
+        this.valorHora = valorHora;
+    }
+
+    public int getNumHoras() {
+        return this.numHoras;
+    }
+
+    public void setNumHoras(int numHoras) {
+        this.numHoras = numHoras;
+    }
+}
